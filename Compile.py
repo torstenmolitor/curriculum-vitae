3bcda70def889f68b73d8a1506b4259fec51214e
emoltor <torsten.molitor@ericsson.com>
2020-09-11 00:19:59 +0200

    compile document class

--- Compile.py
+++ Compile.py
@@ -1,4 +1,5 @@
 import os
+from string import Template
 
 
 class Compiler:
@@ -19,10 +20,35 @@ class LatexMake(Compiler):
 
 
 class Document:
-    def __init__(self, document_name, job_name='', compiler=PdfLatex):
+    def __init__(self, document_name='main', job_name='cv', compiler=PdfLatex):
         self.document_name = document_name
         self.job_name = job_name
         self.compiler = compiler()
 
-    def compile(self):
+    def compile(self, geometry=None):
+        """Compiles the Latex document to a pdf"""
+
+        if geometry is not None:
+            self.set_params(geometry)
+
         os.system(f'{self.compiler.compile_command(job_name=self.job_name)} {self.document_name}')
+
+    def set_params(self, geometry):
+        """Arranges the document with dynamic params"""
+
+        with open(self.document_name + '.tex', 'r') as main:
+            contents_main = main.read()
+
+        new_contents_main = contents_main.replace('geometry-default', 'geometry-dynamic')
+        self.document_name += '-dynamic'
+
+        with open(self.document_name + '.tex', 'w') as main:
+            main.write(new_contents_main)
+
+        with open('dynamic-geometry-template.tex', 'r') as file:
+            dynamic_geometry = Template(template=file.read()).substitute(**geometry)
+
+        with open('dynamic-geometry.tex', 'w') as file:
+            file.write(dynamic_geometry)
+
+
